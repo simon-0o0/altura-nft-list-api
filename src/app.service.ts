8a726a86948d53bc10287a0d5b7680e0413bf3eb
simon <powellgranger0@gmail.com>
2023-03-08 23:41:40 +0800

    add owners

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,7 @@ import {
   FAKE_COLLECTION_NAME,
   HOWRARE_COLLECTIONS_API,
   HOWRARE_MINTS_API,
+  HOWRARE_OWNER_API,
 } from './helper/constants.helper';
 import { logger } from './helper/logger.helper';
 import { ERROR_LIST } from './helper/error.helper';
@@ -36,13 +37,25 @@ export class AppService {
   async getMints(dto: GetMintsDTO) {
     try {
       let mints;
-      const res = (
+      const mintsRes = (
         await axios.get(
           HOWRARE_MINTS_API.replace(FAKE_COLLECTION_NAME, dto.collectionName),
         )
       ).data;
-      if (res.result && res.result.api_response === 'Success')
-        mints = res.result.data;
+      const ownersRes = (
+        await axios.get(
+          HOWRARE_OWNER_API.replace(FAKE_COLLECTION_NAME, dto.collectionName),
+        )
+      ).data;
+      if (
+        mintsRes.result &&
+        mintsRes.result.api_response === 'Success' &&
+        ownersRes.result &&
+        ownersRes.result.api_response === 'Success'
+      ) {
+        mints = mintsRes.result.data;
+        mints.owners = ownersRes.result.data.owners;
+      }
 
       return { statusCode: 200, error: null, message: null, data: mints };
     } catch (e) {
--- src/helper/constants.helper.ts
+++ src/helper/constants.helper.ts
@@ -4,4 +4,7 @@ export const HOWRARE_COLLECTIONS_API =
 export const HOWRARE_MINTS_API =
   'https://api.howrare.is/v0.1/collections/[collectionName]';
 
+export const HOWRARE_OWNER_API =
+  'https://api.howrare.is/v0.1/collections/[collectionName]/owners';
+
 export const FAKE_COLLECTION_NAME = '[collectionName]';
