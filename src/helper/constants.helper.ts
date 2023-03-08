9768ae062f7ebae8d9f4d8577a58d168186661e9
simon <powellgranger0@gmail.com>
2023-03-08 20:20:32 +0800

    update mints api uri

--- src/helper/constants.helper.ts
+++ src/helper/constants.helper.ts
@@ -2,6 +2,6 @@ export const HOWRARE_COLLECTIONS_API =
   'https://api.howrare.is/v0.1/collections';
 
 export const HOWRARE_MINTS_API =
-  'https://api.howrare.is/v0.1/collections/[collectionName]/mints';
+  'https://api.howrare.is/v0.1/collections/[collectionName]';
 
 export const FAKE_COLLECTION_NAME = '[collectionName]';
